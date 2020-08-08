2156a13c4b38a16c1df8561926db84c38059c0d7
Michael Conour <michaelconour@gmail.com>
2020-08-07 18:28:52 -0700

    added utils, reducers, and actions folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,14 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
         <div className="App">
           <Navbar />
@@ -15,6 +20,7 @@ class App extends Component {
           <Route exact path="/login" component={Login} />
         </div>
       </Router>
+      </Provider>
     );
   }
 }
