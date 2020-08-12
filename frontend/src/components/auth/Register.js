26c2e9092b357dfa54739aab5e0e328564a774e9
Michael Conour <michaelconour@gmail.com>
2020-08-11 18:37:01 -0700

    adding lifecycle methods

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -15,7 +15,15 @@ class Register extends Component {
       errors: {}
     };
   }
-componentWillReceiveProps(nextProps) {
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({
         errors: nextProps.errors
